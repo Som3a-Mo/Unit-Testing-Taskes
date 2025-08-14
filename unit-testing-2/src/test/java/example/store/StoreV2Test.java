9c204e0c5125d2a45a6620beb89ac0b044a58318
Esmail Mohamed <depalh19@gmail.com>
2025-08-14 10:24:14 +0300

    Test all buy Scenarios

--- unit-testing-2/src/test/java/example/store/StoreV2Test.java
+++ unit-testing-2/src/test/java/example/store/StoreV2Test.java
@@ -4,6 +4,9 @@ import example.account.AccountManager;
 import example.account.Customer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class StoreV2Test {
@@ -22,7 +25,44 @@ public class StoreV2Test {
         store.buy(product, customer);
 
         // Assert
-        Assertions.assertEquals(3, product.getQuantity());
+        assertEquals(3, product.getQuantity());
     }
 
+    @Test
+    void givenProductWithInsufficientQuantity_whenBuy_thenFailed (){
+
+        AccountManager accountManager = mock(AccountManager.class);
+        when(accountManager.withdraw(any(), anyInt())).thenReturn("success");
+        Store store = new StoreImpl(accountManager);
+        Product product = new Product();
+        product.setQuantity(0);
+        Customer customer = new Customer();
+
+        RuntimeException ex = assertThrows(
+                RuntimeException.class,
+                () -> store.buy(product, customer)
+        );
+
+        assertEquals("Product out of stock", ex.getMessage());
+    }
+
+    @Test
+    void givenProductWithSufficientQuantityAndFailedWithdraw_whenBuy_thenFailed() {
+        // Arrange
+        AccountManager accountManager = mock(AccountManager.class);
+        when(accountManager.withdraw(any(), anyInt())).thenReturn("insufficient account balance");
+        Store store = new StoreImpl(accountManager);
+        Product product = new Product();
+        product.setQuantity(4);
+        Customer customer = new Customer();
+
+        // Act
+        RuntimeException ex = assertThrows(
+                RuntimeException.class,
+                () -> store.buy(product, customer)
+        );
+
+        // Assert
+        assertEquals("Payment failure: insufficient account balance", ex.getMessage());
+    }
 }
